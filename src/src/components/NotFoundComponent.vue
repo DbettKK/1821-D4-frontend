f4ae63ba8125facf136f2f34748a07f46ccf905d
DbettKK <394739138@qq.com>
2020-08-17 18:03:19 +0800

    404页面小优化

--- src/src/components/NotFoundComponent.vue
+++ src/src/components/NotFoundComponent.vue
@@ -1,7 +1,10 @@
 <template>
     <div>
-        <strong>
-            404 not found!
-        </strong>
+        <el-container>
+            <el-main>
+                <div style="font-size:100px;text-align:center;margin:200px 100px 0 100px"><b>404</b></div>
+                <div style="font-size:50px;text-align:center;margin:50px 100px 0 100px">乌乌，页面走丢惹</div>
+            </el-main>
+        </el-container>
     </div>
 </template>
