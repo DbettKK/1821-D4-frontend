7d75e42e5a5ac29f750883b1a6367e9df82dfca6
haosheng055 <54499706+haosheng055@users.noreply.github.com>
2020-08-18 16:26:27 +0800

    增加时间线

--- src/src/components/edit/ue.vue
+++ src/src/components/edit/ue.vue
@@ -99,24 +99,7 @@
     },
     data() {
       return {
-        activities: [{
-          content: '支持使用图标',
-          timestamp: '2018-04-12 20:46',
-          size: 'large',
-          type: 'primary',
-          icon: 'el-icon-more'
-        }, {
-          content: '支持自定义颜色',
-          timestamp: '2018-04-03 20:46',
-          color: '#0bbd87'
-        }, {
-          content: '支持自定义尺寸',
-          timestamp: '2018-04-03 20:46',
-          size: 'large'
-        }, {
-          content: '默认样式的节点',
-          timestamp: '2018-04-03 20:46'
-        }],
+        activities: [],
         collect:false,
         drawer: false,
         CommentdialogFormVisible:false,
@@ -350,7 +333,7 @@
         //this.enterEdit();
         this.GetContents();
         this.get_comment();
-        this.getTimeLine(148);
+        this.getTimeLine(this.file_id);
       }
       else{
         this.$message({
