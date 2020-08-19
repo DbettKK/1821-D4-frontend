4b9bc713f9a4ccd999391ac75bdb6687718040b2
Quasimoodo <63183315+Quasimoodo@users.noreply.github.com>
2020-08-19 17:01:02 +0800

    团队空间重命名取消不了修复

--- src/src/components/user/TeamSpace.vue
+++ src/src/components/user/TeamSpace.vue
@@ -95,7 +95,7 @@
                   </el-form-item>
                 </el-form>
                 <div slot="footer" class="dialog-footer">
-                  <el-button @click="dialog2=false">取消</el-button>
+                  <el-button @click="dialog_rename=false">取消</el-button>
                   <el-button type="primary" @click="renameFile" >确定</el-button>
                 </div>
               </el-dialog>
